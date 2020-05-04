fd753410822b7ea998c6032380ce9faffca4efb7
MOHD IZHAR <44341534+MohdIzhar@users.noreply.github.com>
2020-05-04 22:33:48 +0530

    Convert binary number into decimal number

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,25 @@
+package java_programs;
+
+import java.util.Scanner;
+
+public class Binary_to_Decimal 
+{
+
+	public static void main(String[] args)
+	{
+		int BinaryNumber, sum = 0, i =1, remainder;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the binary number: ");
+		BinaryNumber = input.nextInt();
+		while(BinaryNumber != 0)
+		{
+			remainder = (int)(BinaryNumber % 10);
+			sum = sum + remainder * i;
+			i = i*2;
+			BinaryNumber = BinaryNumber / 10;
+		}
+		System.out.print("Decimal Value is: "+sum);
+		
+	}
+
+}
